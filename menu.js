bfb426b00dbc36f89a7360bb2c4366b2fd95735b
Clarice-COD <clariceboerlijst@gmail.com>
2024-10-27 11:25:44 +0100

    menu test

---
+++ menu.js
@@ -0,0 +1,39 @@
+const openButton = document.querySelector("header > button");
+
+openButton.addEventListener("click", openMenu);
+
+
+// stap 3: voeg in de functie een class toe aan de nav
+function openMenu() {  
+  // zoek de nav op
+  const deNav = document.querySelector("nav");
+  
+  // voeg class toe aan nav
+  deNav.classList.add("toonMenu"); 
+}
+
+
+const sluitButton = document.querySelector("nav button");
+
+// stap 2 - laat die button luisteren naar kliks
+sluitButton.addEventListener("click", sluitMenu);
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu() {
+  const deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
+
+window.onkeydown = handleKeydown;
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    const deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+}
+
+
+
+
+
